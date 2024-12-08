d2067ec9335b3bc2837097fa9e0cd1780ec01d0e
TAT THANG <tatthang204@gmail.com>
2024-12-09 00:43:15 +0700

    ok

---
+++ controllers/MainController.php
@@ -0,0 +1,17 @@
+<?php
+// controllers/MainController.php
+include_once '../models/LoaiVatTuModel.php';
+include_once '../models/VatTuModel.php';
+
+class MainController {
+    public function getLoaiVatTu() {
+        $loaiVatTuModel = new LoaiVatTuModel();
+        return $loaiVatTuModel->getLoaiVatTu();
+    }
+
+    public function getDanhSachVatTu() {
+        $vatTuModel = new VatTuModel();
+        return $vatTuModel->getDanhSachVatTu();
+    }
+}
+?>
--- models/UserModel.php
+++ models/UserModel.php
@@ -12,7 +12,12 @@ class UserModel {
     public function login($username, $password) {
         // Truy vấn cơ sở dữ liệu để tìm người dùng
         $sql = "SELECT * FROM nguoi_dung WHERE ten_nguoi_dung = '$username' AND mat_khau = '$password'";
-        $result = mysqli_query($this->dbConnection, $sql);
+        try{
+            $result = mysqli_query($this->dbConnection, $sql);
+        }catch(Exception $e){
+            return false;
+        }
+       
 
         // Nếu có kết quả, trả về thông tin người dùng, nếu không, trả về false
         if ($result && mysqli_num_rows($result) > 0) {
@@ -22,5 +27,6 @@ class UserModel {
             return false; // Không tìm thấy người dùng
         }
     }
+    
 }
 ?>
