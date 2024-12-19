7e16fa1cdaf25229536e68ccd85eeb3ac60d70bd
TAT THANG <tatthang204@gmail.com>
2024-12-19 22:48:23 +0700

    lmaooooooooooooo

--- controllers/vatTuJson.php
+++ controllers/vatTuJson.php
@@ -1,6 +1,16 @@
 <?php
+//Cho phép bất kỳ nguồn nào (origin) cũng có thể gửi yêu cầu đến tài nguyên trên máy chủ.
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json'); // Xác định kiểu nội dung của phản hồi là JSON.
+header("Access-Control-Allow-Methods: POST");// Chỉ định rằng máy chủ cho phép các yêu cầu HTTP với phương thức DELETE.
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");// Liệt kê tất cả các phương thức HTTP được máy chủ hỗ trợ.
+//Cho phép các tiêu đề HTTP tùy chỉnh mà client có thể gửi trong yêu cầu (request).
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Allow-Origin");
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $data = [
+    $vatTu_array = [];
+    $vatTu_array['dataToCreateVatTu'] = [];
+    $vatTu_item = array(
         'ma_vat_tu' => $_POST['ma_vat_tu'],
         'ten_vat_tu' => $_POST['ten_vat_tu'],
         'mo_ta' => $_POST['mo_ta'],
@@ -10,9 +20,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'so_luong_toi_thieu' => $_POST['so_luong_toi_thieu'],
         'so_luong_ton' => $_POST['so_luong_ton'],
         'ma_loai_vat_tu' => $_POST['loai_vat_tu']
-    ];
-    echo json_encode($data);
-    header('Location: ../index.php');
+    );
+    array_push( $vatTu_array['dataToCreateVatTu'], $vatTu_item);
 }
-
+echo json_encode($vatTu_array);
 ?>
