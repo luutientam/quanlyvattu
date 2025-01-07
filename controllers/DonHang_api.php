3901d3fa208f0c8427065272430232ca8e61676d
dimz1506 <khaikhainguyennguyen219118156@gmail.com>
2025-01-07 20:59:16 +0700

    sua cho anh

--- controllers/DonHang_api.php
+++ controllers/DonHang_api.php
@@ -6,6 +6,8 @@ header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Access-Con
 
 require_once "../models/db.php";
 require_once "../models/DonHangModel.php";
+require_once "../models/GetDuLieu.php";
+
 
 $db = new db();
 $connect = $db->connect();
@@ -16,30 +18,42 @@ if (!$connect) {
 }
 
 $donHangModel = new DonHangModel($connect);
+$getDuLieu = new GetDuLieu();
+
 
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
 switch ($requestMethod) {
     case 'GET':
-        $keyword = isset($_GET['keyword']) ? $_GET['keyword'] : '';
-        $read = $donHangModel->getDonHang($keyword);
-        $num = count($read);
-        if ($num > 0) {
-            $donhang_array = [];
-            $donhang_array['data'] = [];
-            foreach ($read as $row) {
-                $donhang_item = array(
-                    'ma_don_hang' => $row['ma_don_hang'],
-                    'ten_khach_hang' => $row['ten_khach_hang'],
-                    'trang_thai' => $row['trang_thai'],
-                    'ngay_dat_hang' => $row['ngay_dat_hang'],
-                    'ma_nhan_vien' => $row['ma_nhan_vien']
-                );
-                array_push($donhang_array['data'], $donhang_item);
+        if (isset($_GET['action']) && $_GET['action'] === 'getGiaVatTu') {
+            $maVatTu = $_GET['ma_vat_tu'];
+            $gia = $getDuLieu->getGiaVatTu($maVatTu);
+            if ($gia !== null) {
+                echo json_encode(['status' => 'success', 'gia' => $gia]);
+            } else {
+                echo json_encode(['status' => 'error', 'message' => 'Không tìm thấy giá sản phẩm']);
             }
-            echo json_encode($donhang_array);
         } else {
-            echo json_encode(['message' => 'Không tìm thấy đơn hàng']);
+            $keyword = isset($_GET['keyword']) ? $_GET['keyword'] : '';
+            $read = $donHangModel->getDonHang($keyword);
+            $num = count($read);
+            if ($num > 0) {
+                $donhang_array = [];
+                $donhang_array['data'] = [];
+                foreach ($read as $row) {
+                    $donhang_item = array(
+                        'ma_don_hang' => $row['ma_don_hang'],
+                        'ten_khach_hang' => $row['ten_khach_hang'],
+                        'trang_thai' => $row['trang_thai'],
+                        'ngay_dat_hang' => $row['ngay_dat_hang'],
+                        'ma_nhan_vien' => $row['ma_nhan_vien']
+                    );
+                    array_push($donhang_array['data'], $donhang_item);
+                }
+                echo json_encode($donhang_array);
+            } else {
+                echo json_encode(['message' => 'Không tìm thấy đơn hàng']);
+            }
         }
         break;
 
@@ -69,22 +83,6 @@ switch ($requestMethod) {
             echo json_encode(['status' => 500, 'message' => 'Lỗi khi cập nhật đơn hàng']);
         }
         break;
-
-    // case 'DELETE':
-    //     $inputData = json_decode(file_get_contents("php://input"), true);
-    //     if (empty($inputData) || !isset($inputData['ma_don_hang'])) {
-    //         echo json_encode(['message' => 'Dữ liệu không hợp lệ']);
-    //         exit();
-    //     }
-
-    //     $ma_don_hang = $inputData['ma_don_hang'];
-    //     $response = $donHangModel->delete($ma_don_hang);
-    //     if ($response) {
-    //         echo json_encode(['status' => 200, 'message' => 'Xóa đơn hàng thành công']);
-    //     } else {
-    //         echo json_encode(['status' => 500, 'message' => 'Lỗi khi xóa đơn hàng']);
-    //     }
-    //     break;
 
     default:
         http_response_code(405);
